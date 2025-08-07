e508c8ee8c3f31a4fdd383e0bb691ed5ac1c3386
Joshua Hsu <jshhsu168@gmail.com>
2025-08-07 10:08:00 -0700

    Created login and register forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
+import Homepage from './components/Homepage';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
-      <h1>Welcome to FSU</h1>
-    </>
+    <Router>
+      <header>
+        <h1>MEV</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
