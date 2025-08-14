86a694592843386a383e2e5c2c11b4857b16a725
Fabiana Carvalho <fabiana.acarvalho@outlook.com>
2025-08-13 21:09:19 -0300

    additional changes

---
+++ src/components/Converter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Converter(props) {
+  const { currencyOptions, selectedCurrency } = props;
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select value={selectedCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
