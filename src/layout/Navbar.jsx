74c286b473b852953cb83af5603e73515ba68754
Fabiana Carvalho <fabiana.acarvalho@outlook.com>
2025-08-19 20:11:16 -0300

    Update Navbar.jsx
    change "react-router-dom" to "react-router"

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 
 import "../styles/components/navbar.css";
