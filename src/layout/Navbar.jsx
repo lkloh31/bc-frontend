2c25833f408e53ea9ec58ec32280065f1238afeb
Jpgranger <joey1027@live.com>
2025-08-19 12:58:20 -0500

    Fix Name

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar() {
               Journal
             </Link>
             <Link to="/brain" onClick={closeMenu}>
-            Brain Games
+              Games
             </Link>
             <a href="#" onClick={closeMenu}>
               +
